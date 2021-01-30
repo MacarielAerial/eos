4a081ee2f30e51ac9437e7444f744ec2fb18fd59
MacarielAerial <yyu.mam2020@london.edu>
2021-01-30 19:02:40 +0000

    Fix gitignore issue

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -6,11 +6,12 @@ table_name: str = "user"
 path_from_wd: str = "tests/data/e2e/user.db"
 
 def check_db_exist(path):
-    return isfile(path)
+    db_exist: bool = isfile(path)
+    if not db_exist:
+        raise FileNotFoundError(f"{path} does not exist")
 
 def test_e2e():
-    if not check_db_exist(realpath(path_from_wd)):
-        raise FileNotFoundError(f"{realpath(path_from_wd)} does not exist")
+    check_db_exist(path = realpath(path_from_wd))
     conn_str: str = "sqlite:///" + realpath(path_from_wd)
     G: Graph = orchestrator(conn_str = conn_str, table_name = table_name)
     assert isinstance(G, Graph)
