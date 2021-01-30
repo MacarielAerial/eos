cedf94e3502a176b9a55f0c828202ac6945b4283
MacarielAerial <yyu.mam2020@london.edu>
2021-01-30 18:43:48 +0000

    Fixed connection string for sqlite

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,11 +1,12 @@
 from eos.orchestrator import orchestrator
-
+from os.path import realpath
 from networkx import Graph
 
-conn_str: str = r"sqlite:///tests/data/e2e/user.db"
-table_name = "user"
+table_name: str = "user"
+path_from_wd: str = "tests/data/e2e/user.db"
 
 
 def test_e2e():
-    G:Graph = orchestrator(conn_str = conn_str, table_name = table_name)
+    conn_str: str = "sqlite:///" + realpath(path_from_wd)
+    G: Graph = orchestrator(conn_str = conn_str, table_name = table_name)
     assert isinstance(G, Graph)
