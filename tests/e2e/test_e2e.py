c38be290e461d6a8fd0ce513edbdf7f15a16a080
MacarielAerial <yyu.mam2020@london.edu>
2021-01-30 18:55:21 +0000

    debug test

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,12 +1,16 @@
 from eos.orchestrator import orchestrator
-from os.path import realpath
+from os.path import realpath, isfile
 from networkx import Graph
 
 table_name: str = "user"
 path_from_wd: str = "tests/data/e2e/user.db"
 
+def check_db_exist(path):
+    return isfile(path)
 
 def test_e2e():
+    if not check_db_exist(realpath(path_from_wd)):
+        raise FileNotFoundError(f"{realpath(path_from_wd)} does not exist")
     conn_str: str = "sqlite:///" + realpath(path_from_wd)
     G: Graph = orchestrator(conn_str = conn_str, table_name = table_name)
     assert isinstance(G, Graph)
